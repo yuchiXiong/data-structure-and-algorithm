1690f52b25b2c491b4a9959c9a2c0b2c73ae37ae
yuchiXiong <yuchi.xiong@foxmail.com>
2021-12-10 18:05:23 +0800

    [基础] 调整希尔排序的算法逻辑，修复性能问题；

--- 0. 基础算法/1. 排序/shells_sort.cpp
+++ 0. 基础算法/1. 排序/shells_sort.cpp
@@ -2,12 +2,19 @@
 #include "../../tools/tools.cpp"
 using namespace std;
 
+/**
+ * 希尔排序
+ * 希尔排序是插入排序的一种更高效的改进版本。
+ * note1:
+ *  希尔排序是以n为增量的插入排序，在插入排序中，每次从对一个大于2的数组做插入排序时需要遍历多次，但如果数组只有2个元素，则只需要一次对比
+ * 
+ */
 void shells_sort(int *list, int size)
 {
-  int incre = size / 100;
+  int incre = size / 2;
   while (incre > 0)
   {
-    for (int i = 1; i < size; i += incre)
+    for (int i = incre; i < size; i++)
     {
       int current = list[i];
       int j = i;
@@ -17,17 +24,15 @@ void shells_sort(int *list, int size)
       }
       list[j] = current;
     }
-    incre /= 100;
+    incre /= 2;
   }
 };
 
 int main()
 {
-  int size = 900000;
+  int size = 100000;
   int *arr = ArrayTools::random(size);
 
-  // cout << "原始数组" << endl;
-  // ArrayTools::prinf_arr(arr, size);
   clock_t start, end;
 
   start = clock();
@@ -36,7 +41,5 @@ int main()
 
   cout << (end - start) / 1000.0 << endl;
 
-  // cout << "排序后" << endl;
-  // ArrayTools::prinf_arr(arr, size);
   return 0;
 }
