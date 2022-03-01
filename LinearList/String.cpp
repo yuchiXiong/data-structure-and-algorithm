b4653754611b481147c0bfe8daa775baf74c2252
yuchiXiong <yuchi.xiong@foxmail.com>
2022-03-01 17:44:37 +0800

    feat: 简单实现一个不完整的字符串类与 BF 查询算法；

---
+++ LinearList/String.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class String
+{
+private:
+  char *_str;
+  int _len;
+
+public:
+  String(char *str = NULL)
+  {
+    _str = str;
+    char *p = str;
+    while (*p != '\0')
+    {
+      _len++;
+      p++;
+    }
+  }
+  int length()
+  {
+    return _len;
+  }
+  friend ostream &operator<<(ostream &output,
+                             const String *D)
+  {
+    output << D->_str;
+    return output;
+  }
+  int indexOf_BF(const char *str)
+  {
+    int result = -1;
+    for (int i = 0; i < _len; i++)
+    {
+      int i_dup = i;
+      if (_str[i] == str[0])
+      {
+        int j = 1;
+        while (str[j] != '\0' && _str[i + 1] == str[j])
+        {
+          i++;
+          j++;
+        }
+        if (str[j] == '\0')
+        {
+          result = i + 1 - j;
+          break;
+        }
+        else
+        {
+          i = i_dup;
+        }
+      }
+    }
+    return result;
+  }
+};
+
+int main()
+{
+  char str[] = "Hello World";
+  String *s = new String(str);
+
+  // cout << s << endl;
+  // cout << s->length() << endl;
+
+  cout << s->indexOf_BF("el") << endl;  // 1
+  cout << s->indexOf_BF("lo") << endl;  // 3
+  cout << s->indexOf_BF("rl") << endl;  // 8
+  cout << s->indexOf_BF("ld") << endl;  // 9
+  cout << s->indexOf_BF("rld") << endl; // 8
+}
