21c73a40d49c6eacb52f74c6d8d827bc06736744
yuchiXiong <yuchi.xiong@foxmail.com>
2022-03-02 18:34:07 +0800

    feat: 完成 KMP 算法基于 dp 递推过程的计算，算法暂未完成;

--- LinearList/String.cpp
+++ LinearList/String.cpp
@@ -55,6 +55,57 @@ public:
     }
     return result;
   }
+  int indexOf_KMP(const char *pattern)
+  {
+    int len = 1;
+    char p = pattern[0];
+    while (p != '\0')
+    {
+      len++;
+      p = pattern[len];
+    }
+
+    int dp[len], i = 1, k = 0;
+    dp[0] = 0;
+    while (i < len)
+    {
+      if (pattern[k] == pattern[i])
+      {
+        dp[i++] = ++k;
+      }
+      else if (k != 0)
+      {
+        k = dp[--k];
+      }
+      else
+      {
+        dp[i++] = 0;
+      }
+    }
+
+    int result = -1, j = 0;
+    i = 0;
+    for (; i < _len; i++)
+    {
+      if (_str[i] == pattern[j])
+      {
+        j++;
+        if (pattern[j] == '\0')
+        {
+          result = i - len + 1;
+          break;
+        }
+      }
+      else
+      {
+        if (j != 0)
+          j = dp[j];
+        continue;
+      }
+    }
+
+    return result;
+  }
 };
 
 int main()
@@ -65,9 +116,15 @@ int main()
   // cout << s << endl;
   // cout << s->length() << endl;
 
-  cout << s->indexOf_BF("el") << endl;  // 1
-  cout << s->indexOf_BF("lo") << endl;  // 3
-  cout << s->indexOf_BF("rl") << endl;  // 8
-  cout << s->indexOf_BF("ld") << endl;  // 9
-  cout << s->indexOf_BF("rld") << endl; // 8
+  // cout << s->indexOf_BF("el") << endl;  // 1
+  // cout << s->indexOf_BF("lo") << endl;  // 3
+  // cout << s->indexOf_BF("rl") << endl;  // 8
+  // cout << s->indexOf_BF("ld") << endl;  // 9
+  // cout << s->indexOf_BF("rld") << endl; // 8
+
+  // cout << s->indexOf_KMP("el") << endl; // 1
+  cout << s->indexOf_KMP("lo") << endl; // 3
+  // cout << s->indexOf_KMP("rl") << endl;  // 8
+  // cout << s->indexOf_KMP("ld") << endl;  // 9
+  // cout << s->indexOf_KMP("rld") << endl; // 8
 }
