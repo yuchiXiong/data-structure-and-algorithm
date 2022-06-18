343ad164fa63144e46cfef368257a00573571d2e
yuchiXiong <yuchi.xiong@foxmail.com>
2022-06-18 14:37:25 +0800

    [leetcode] 144. 二叉树的前序遍历

---
+++ leetcode/144(简单)/1. 二叉树的前序遍历.cpp
@@ -0,0 +1,62 @@
+#include "iostream"
+#include "vector"
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  vector<int> preorderTraversal(TreeNode *root)
+  {
+    vector<int> results;
+    if (root == nullptr)
+      return results;
+    _preorderTraversal(root, &results);
+    return results;
+  }
+
+  void _preorderTraversal(TreeNode *root, vector<int> *list)
+  {
+    list->push_back(root->val);
+    if (root->left)
+      _preorderTraversal(root->left, list);
+    if (root->right)
+      _preorderTraversal(root->right, list);
+  }
+};
+
+int main()
+{
+
+  TreeNode *tn = new TreeNode(1, nullptr, new TreeNode(3, nullptr, nullptr));
+  Solution *s = new Solution();
+
+  vector<int> results = s->preorderTraversal(tn);
+
+  for (int i = 0; i < results.size(); i++)
+  {
+    cout << results.at(i) << " ";
+  }
+
+  cout << endl;
+}
