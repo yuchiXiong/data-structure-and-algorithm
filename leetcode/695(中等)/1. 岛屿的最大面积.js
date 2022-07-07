63659b20643b1e6b7b8bfb59efd283d21257868a
yuchiXiong <yuchi.xiong@foxmail.com>
2022-07-07 17:35:45 +0800

    [leetcode] 695. 岛屿的最大面积

---
+++ leetcode/695(中等)/1. 岛屿的最大面积.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+  let max = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        max = Math.max(max, dfs(grid, i, j));
+      }
+    }
+  }
+
+  return max;
+};
+
+const dfs = (grid, i, j) => {
+  if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return 0;
+  if (grid[i][j] === 0 || grid[i][j] === 2) return 0;
+  grid[i][j] = 2;
+  return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+}
+
+// 6
+console.log(maxAreaOfIsland([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]));
