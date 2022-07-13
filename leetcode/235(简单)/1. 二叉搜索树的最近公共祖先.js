6fcf0e5aebcd68c8c9c808db37eca26039b6b9c1
yuchiXiong <yuchi.xiong@foxmail.com>
2022-07-13 13:01:52 +0800

    [leetcode] 235. 二叉搜索树的最近公共祖先

---
+++ leetcode/235(简单)/1. 二叉搜索树的最近公共祖先.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null) {
+    return null;
+  }
+
+  if (p.val === root.val || q.val === root.val) {
+    return root;
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else {
+    return root;
+  }
+
+};
