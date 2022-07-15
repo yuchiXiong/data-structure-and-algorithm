d24c77406e601cf7edfc6ee41a9d642c16d15fc9
yuchiXiong <yuchi.xiong@foxmail.com>
2022-07-15 18:23:35 +0800

    [leetcode] 136. 只出现一次的数字

---
+++ leetcode/136(简单)/1. 只出现一次的数字.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class Solution
+{
+public:
+  int singleNumber(vector<int> &nums)
+  {
+    int ans = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+      ans = ans ^ nums[i];
+    }
+
+    return ans;
+  }
+};
