47965f48ec52c5c7c88a5fd01034e752dacfcccb
yuchiXiong <yuchi.xiong@foxmail.com>
2022-07-21 08:47:41 +0800

    [leetcode] 814. 二叉树剪枝

---
+++ leetcode/814(中等)/1. 二叉树剪枝.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+  TreeNode *pruneTree(TreeNode *root)
+  {
+    return _prunTree(root);
+  }
+
+  TreeNode *_prunTree(TreeNode *root)
+  {
+    if (root == nullptr)
+      return nullptr;
+
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+
+    if (root->val == 0 && root->left == nullptr && root->right == nullptr)
+    {
+      return nullptr;
+    }
+    else
+    {
+      return root;
+    }
+  }
+};
