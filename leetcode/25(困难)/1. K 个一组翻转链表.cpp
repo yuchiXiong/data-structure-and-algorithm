c70e3647c835ec0e6a50c4485e9ac2eaf28104f2
yuchiXiong <yuchi.xiong@foxmail.com>
2022-08-30 22:23:41 +0800

    [leetcode] 25. K 个一组翻转链表

---
+++ leetcode/25(困难)/1. K 个一组翻转链表.cpp
@@ -0,0 +1,61 @@
+#include "iostream"
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *p;
+  Solution()
+  {
+    p = NULL;
+  }
+  ListNode *reverseKGroup(ListNode *head, int k)
+  {
+    ListNode *cur = head;
+    for (int i = 1; i <= k; i++)
+    {
+      if (cur == NULL)
+        return head;
+      cur = cur->next;
+    }
+    ListNode *last = reverseList(head, k);
+
+    head->next = reverseKGroup(head->next, k);
+
+    return last;
+  }
+
+  ListNode *reverseList(ListNode *head, int n)
+  {
+    if (n == 1)
+    {
+      p = head->next;
+      return head;
+    }
+
+    ListNode *last = reverseList(head->next, n - 1);
+    head->next->next = head;
+    head->next = p;
+
+    return last;
+  }
+};
