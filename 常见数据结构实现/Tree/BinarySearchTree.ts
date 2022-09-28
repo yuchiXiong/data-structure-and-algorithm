855233adbcd293213e32110ab5e7c4caf1cef613
yuchiXiong <yuchi.xiong@foxmail.com>
2022-09-28 16:43:51 +0800

    feat: 调整了 BST 删除方法的实现与规范，添加了一些用于测试的代码

--- 常见数据结构实现/Tree/BinarySearchTree.ts
+++ 常见数据结构实现/Tree/BinarySearchTree.ts
@@ -49,6 +49,8 @@ export default class BinarySearchTree<T> extends BinaryTree<T> {
   }
 
   contain(val: T): boolean {
+    if (val === this.val) return true;
+
     if (val < this.val) {
       return this.left ? this.left.contain(val) : false
     } else {
@@ -56,31 +58,67 @@ export default class BinarySearchTree<T> extends BinaryTree<T> {
     }
   }
 
-  getMax(): T {
-    if (!this.right) return this.val;
+  getMax(): BinarySearchTree<T> {
+    if (!this.right) return this;
     return this.right.getMax();
   }
 
-  getMin(): T {
-    if (!this.left) return this.val;
+  getMin(): BinarySearchTree<T> {
+    if (!this.left) return this;
     return this.left.getMin();
   }
 
+  remove(val: T): BinarySearchTree<T> | null {
+    if (!this.contain(val)) return this;
+
+    if (val < this.val) {
+      this.left && (this.left = this.left?.remove(val));
+    } else if (val > this.val) {
+      this.right && (this.right = this.right?.remove(val));
+    } else {
+      if (!this.left && !this.remove) return null;
+      if (!this.left) return this.right;
+      if (!this.right) return this.left;
+
+      // 选择一个新的节点当作当前节点
+      const min = this.right.getMin();
+      const next = new BinarySearchTree<T>(min.val);
+
+      next.right = this.right.remove(min.val);
+      next.left = this.left;
+      next._size = this.size();
+
+      return next;
+    }
+
+    return this;
+  }
 }
 
-const mockData: number[] = new Array(9999).fill(0).map(_ => Math.floor(Math.random() * 9999))
-const bst = new BinarySearchTree<number>(6);
-
-mockData.forEach(i => bst.put(i));
-
-mockData.unshift(6);
-
-bst.dfs('in');
+const mockData: number[] = [...new Set(new Array(9999).fill(0).map(_ => Math.floor(Math.random() * 9999)))]
+let bst: BinarySearchTree<number> | null = null;
+mockData.forEach((cur, i) => {
+  if (i === 0) {
+    bst = new BinarySearchTree<number>(cur);
+  } else {
+    bst!.put(cur);
+  }
+});
 
 const sorted = mockData.sort((a, b) => a - b);
-console.log("验证有序性: ", bst.dfs('in').join('-') === sorted.join('-'));
-console.log("当前BST大小: ", bst._size);
-console.log("验证顺次:", sorted.every((item, index) => bst.get(index) === item));
-console.log("验证最大值", bst.getMax() === sorted[sorted.length - 1]);
-console.log("验证最小值", bst.getMin() === sorted[0]);
+console.log("验证有序性: ", bst!.dfs('in').join('-') === sorted.join('-'));
+console.log("当前BST大小: ", bst!._size);
+console.log("验证顺次:", sorted.every((item, index) => bst!.get(index) === item));
+console.log("验证最大值", bst!.getMax().val === sorted[sorted.length - 1]);
+console.log("验证最小值", bst!.getMin().val === sorted[0]);
 
+while (bst !== null) {
+  const randomIndex = Math.floor(Math.random() * mockData.length);
+  bst = (bst as BinarySearchTree<number>).remove(mockData[randomIndex]);
+  mockData.splice(randomIndex, 1);
+
+  if ((bst?.dfs('in') || []).join('|') !== mockData.sort((a, b) => a - b).join('|')) {
+    console.log('删除验证失败！')
+    break;
+  }
+}
